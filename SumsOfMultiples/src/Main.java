494b151f9f0612a967bdb1694704e26ef1f7f50a
Tiffany Obi <tiffanyobi123@gmail.com>
2024-07-15 11:45:01 -0400

    adding JS and Java Sums of Mulitples

---
+++ SumsOfMultiples/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println();
+        System.out.println("Sum Of Multiples");
+
+        System.out.println(SumOfMultiples.sumOfMultiples(7)); //21
+        System.out.println(SumOfMultiples.sumOfMultiples(10));//40
+        System.out.println(SumOfMultiples.sumOfMultiples(9));//30
+        System.out.println(SumOfMultiples.sumOfMultiples(15));//81
+    }
+}
